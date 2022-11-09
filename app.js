7d70477d7e9ca2e3b4617ae06d9eb2f7816676bf
Jeric <marianovjeric@gmail.com>
2022-11-09 10:42:03 -0800

    first for loop, print 1 - 100

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-let count = 1;
+// let count = 1;
 
-while (count <= 100) {
-    console.log(count)
-    count = count + 1;
+// while (count <= 100) {
+//     console.log(count)
+//     count = count + 1;
+// }
+
+for (let i = 0; i < 100; i++) {
+    console.log(i + 1)
 }
