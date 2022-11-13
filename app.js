4e9768d563363bdfb84818e8adf38b1fec133535
Jeric <marianovjeric@gmail.com>
2022-11-12 20:47:12 -0800

    filter method example

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let arr = [20, 30, 40, 50, 100]
 
-console.log(arr[0])
+let newArr = arr.filter( (element) => {
+    console.log(element)
+    if (element < 50) {
+        return true
+    }
+} )
 
-console.log(arr[arr.length-1])
-
-arr.push(200)
-
-console.log(arr)
+console.log(newArr)
