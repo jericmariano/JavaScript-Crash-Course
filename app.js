63b7647dca5d5c69af75cbfc2e540deaa519f300
Jeric <marianovjeric@gmail.com>
2022-11-12 20:37:42 -0800

    first array and push example

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const convertCelsiusToFahrenheit = (celsius) => {
-    return celsius * 1.8 + 32
-}
+let arr = [20, 30, 40, 50, 100]
 
-console.log(convertCelsiusToFahrenheit(0))
+console.log(arr[0])
+
+console.log(arr[arr.length-1])
+
+arr.push(200)
+
+console.log(arr)
