3ac0e4b4504d90d824203afcd28ed6ea5afa9943
Jeric <marianovjeric@gmail.com>
2022-11-12 21:27:29 -0800

    filter/callback practice problem

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-let arr = [20, 30, 40, 50, 100]
+// let arr = [20, 30, 40, 50, 100]
 
-let newArr = arr.filter(element => element < 50)
+// let newArr = arr.filter(element => element < 50)
 
-console.log(newArr)
+// console.log(newArr)
+
+let grades = ['FAIL', 'FAIL', 'B']
+
+console.log(grades.filter( (element) => element != 'FAIL'))
