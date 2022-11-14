25648781a5b4e2073924f8560abc0db90e7022c6
Jeric <marianovjeric@gmail.com>
2022-11-13 21:47:53 -0800

    login function

--- app.js
+++ app.js
@@ -2,15 +2,45 @@ let users = [
     {
     username: 'Jeric', 
     email: 'jericmariano08@gmail.com',
+    password: 'test123',
     subscriptionStatus: 'VIP',
-    discordId: 'jmar'
+    discordId: 'jmar',
+    lessonsCompleted: [0, 1]
     },
     {
     username: 'Derp', 
     email: 'Derp@gmail.com',
+    password: 'derp123',
     subscriptionStatus: 'VIP',
-    discordId: 'derp'
+    discordId: 'derp',
+    lessonsCompleted: [0, 1, 2, 3]
+    },
+    {
+    username: 'Zen', 
+    email: 'zen@gmail.com',
+    password: 'zen123',
+    subscriptionStatus: 'VIP',
+    discordId: 'zen',
+    lessonsCompleted: [0, 1, 2, 3]
     }
 ]
 
-console.log(users[0])
+function logIn (email, password) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            console.log(users[i])
+            if (users[i].password === password) {
+                console.log('log the user in - the details are correct')
+            }
+            else {
+                console.log('password is incorrect - try again')
+            }
+            return
+        }
+    }
+    console.log('could not find an email that matches')
+}
+
+console.log(users[0].lessonsCompleted.map(elem => elem * 3))
+
+logIn('doesnotexist@gmail.com', 'merp')
