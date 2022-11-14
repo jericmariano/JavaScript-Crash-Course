49d15c80069f1d5b3c263f14d8328c6e06cc2fd6
Jeric <marianovjeric@gmail.com>
2022-11-13 21:25:41 -0800

    for loop instead of map example solved

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 let arr = [1, 5, 10, 3];
 
-let newArr = arr.map( element => element * 100)
+// let newArr = arr.map( element => element * 100)
+
+// console.log(newArr)
+
+let newArr = []
+
+for (let i = 0; i < arr.length; i++) {
+    newArr[i] = arr[i] * 100 // or newArr.push(arr[i] * 100)
+}
 
 console.log(newArr)
