904c0c84c4d0231c906b570e8925395a4e2a78e3
Jeric <marianovjeric@gmail.com>
2022-11-13 21:35:45 -0800

    object example

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-let arr = [1, 5, 10, 3];
-
-// let newArr = arr.map( element => element * 100)
-
-// console.log(newArr)
-
-let newArr = []
-
-for (let i = 0; i < arr.length; i++) {
-    newArr[i] = arr[i] * 100 // or newArr.push(arr[i] * 100)
+let user = {
+    username: 'Jeric', 
+    email: 'jericmariano08@gmail.com',
+    subscriptionStatus: 'VIP',
+    discordId: 'jmar'
 }
 
-console.log(newArr)
+console.log(user.username)
+
+console.log(user.subscriptionStatus)
